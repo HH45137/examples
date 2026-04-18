527cbaff6c732b6943d308bd34ad0e1e67da3e38
RISC-1145 <wtpp647@gmail.com>
2026-04-18 14:00:11 +0800

    Update model.py

--- super_resolution/model.py
+++ super_resolution/model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.init as init
+import torch.nn.functional as F
 
 
 class Net(nn.Module):
@@ -9,15 +10,18 @@ class Net(nn.Module):
 
         self.act = nn.LeakyReLU(0.2)
         
-        self.conv1 = nn.Conv2d(1, 64, (3, 3), (1, 1), (1, 1))
-        self.conv2 = nn.Conv2d(64, 128, (3, 3), (1, 1), (1, 1))
-        self.conv3 = nn.Conv2d(128, 128, (3, 3), (1, 1), (1, 1))
-        self.conv4 = nn.Conv2d(128, 64, (3, 3), (1, 1), (1, 1))
-        self.conv5 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
-        self.conv6 = nn.Conv2d(32, upscale_factor**2, (3, 3), (1, 1), (1, 1))
-
+        pad = 'same'
+        kernel_size = (4, 4)
+        stride = (1, 1)
+        
+        self.conv1 = nn.Conv2d(1, 64, kernel_size, stride, pad)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size, stride, pad)
+        self.conv3 = nn.Conv2d(128, 128, kernel_size, stride, pad)
+        self.conv4 = nn.Conv2d(128, 64, kernel_size, stride, pad)
+        self.conv5 = nn.Conv2d(64, 32, kernel_size, stride, pad)
+        self.conv6 = nn.Conv2d(32, upscale_factor**2, kernel_size, stride, pad)
+        
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
-
         self._initialize_weights()
 
     def forward(self, x):
@@ -28,12 +32,15 @@ class Net(nn.Module):
         x = self.act(self.conv5(x))
         x = self.pixel_shuffle(self.conv6(x))
         x = torch.sigmoid(x)
+
         return x
 
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                if m in [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5]:
+                if m == self.conv6:
+                    init.orthogonal_(m.weight, gain=0.1)
+                elif m in [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5]:
                     init.orthogonal_(m.weight, init.calculate_gain('leaky_relu', 0.2))
                 else:
                     init.orthogonal_(m.weight)
