16bc2cddabde15319f8dcc44ebc2e9bbff31fec4
RISC-1145 <wtpp647@gmail.com>
2026-04-18 18:08:02 +0800

    Update model.py

--- super_resolution/model.py
+++ super_resolution/model.py
@@ -15,10 +15,11 @@ class Net(nn.Module):
         stride = (1, 1)
         
         self.conv1 = nn.Conv2d(3, 64, kernel_size, stride, pad)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size, stride, pad)
-        self.conv3 = nn.Conv2d(128, 128, kernel_size, stride, pad)
-        self.conv4 = nn.Conv2d(128, 64, kernel_size, stride, pad)
-        self.conv5 = nn.Conv2d(64, 3 * (upscale_factor ** 2), kernel_size, stride, pad)
+        self.conv2 = nn.Conv2d(64, 96, kernel_size, stride, pad)
+        self.conv3 = nn.Conv2d(96, 128, kernel_size, stride, pad)
+        self.conv4 = nn.Conv2d(128, 96, kernel_size, stride, pad)
+        self.conv5 = nn.Conv2d(96, 64, kernel_size, stride, pad)
+        self.conv6 = nn.Conv2d(64, 3 * (upscale_factor ** 2), kernel_size, stride, pad)
         
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
         self._initialize_weights()
@@ -28,16 +29,17 @@ class Net(nn.Module):
         x = self.act(self.conv2(x))
         x = self.act(self.conv3(x))
         x = self.act(self.conv4(x))
-        x = self.pixel_shuffle(self.conv5(x))
+        x = self.act(self.conv5(x))
+        x = self.pixel_shuffle(self.conv6(x))
         x = torch.sigmoid(x)
         return x
 
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                if m == self.conv5:
+                if m == self.conv6:
                     init.orthogonal_(m.weight, gain=0.1)
-                elif m in [self.conv1, self.conv2, self.conv3, self.conv4]:
+                elif m in [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5]:
                     init.orthogonal_(m.weight, init.calculate_gain('leaky_relu', 0.2))
                 else:
                     init.orthogonal_(m.weight)
