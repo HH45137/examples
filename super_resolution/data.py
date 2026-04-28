1d14cdf37f91d46a42547f61885ee65b7d4738fa
RISC-1145 <wtpp647@gmail.com>
2026-04-28 14:30:04 +0800

    Update model arch

--- super_resolution/data.py
+++ super_resolution/data.py
@@ -3,32 +3,10 @@ from os import makedirs, remove
 from six.moves import urllib
 import tarfile
 from torchvision.transforms import Compose, CenterCrop, ToTensor, Resize
-
 from dataset import DatasetFromFolder
 
 
-def download_bsd300(dest="dataset"):
-    output_image_dir = join(dest, "BSDS300/images")
-
-    if not exists(output_image_dir):
-        makedirs(dest)
-        url = "http://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/BSDS300-images.tgz"
-        print("downloading url ", url)
-
-        data = urllib.request.urlopen(url)
-
-        file_path = join(dest, basename(url))
-        with open(file_path, 'wb') as f:
-            f.write(data.read())
-
-        print("Extracting data")
-        with tarfile.open(file_path) as tar:
-            for item in tar:
-                tar.extract(item, dest)
-
-        remove(file_path)
-
-    return output_image_dir
+CROP_SIZE_4K = 1080
 
 
 def calculate_valid_crop_size(crop_size, upscale_factor):
@@ -52,7 +30,7 @@ def target_transform(crop_size):
 
 def get_training_set(upscale_factor, root_dir):
     train_dir = join(root_dir, "train")
-    crop_size = calculate_valid_crop_size(512, upscale_factor)
+    crop_size = calculate_valid_crop_size(CROP_SIZE_4K, upscale_factor)
 
     return DatasetFromFolder(train_dir,
                              input_transform=input_transform(crop_size, upscale_factor),
@@ -61,7 +39,7 @@ def get_training_set(upscale_factor, root_dir):
 
 def get_test_set(upscale_factor, root_dir):
     test_dir = join(root_dir, "test")
-    crop_size = calculate_valid_crop_size(512, upscale_factor)
+    crop_size = calculate_valid_crop_size(CROP_SIZE_4K, upscale_factor)
 
     return DatasetFromFolder(test_dir,
                              input_transform=input_transform(crop_size, upscale_factor),
--- super_resolution/model.py
+++ super_resolution/model.py
@@ -9,37 +9,34 @@ class Net(nn.Module):
         super(Net, self).__init__()
 
         self.act = nn.LeakyReLU(0.2)
-        
+
         pad = (1, 1)
         kernel_size = (3, 3)
         stride = (1, 1)
-        
-        self.conv1 = nn.Conv2d(3, 64, kernel_size, stride, pad)
-        self.conv2 = nn.Conv2d(64, 96, kernel_size, stride, pad)
-        self.conv3 = nn.Conv2d(96, 128, kernel_size, stride, pad)
-        self.conv4 = nn.Conv2d(128, 96, kernel_size, stride, pad)
-        self.conv5 = nn.Conv2d(96, 64, kernel_size, stride, pad)
-        self.conv6 = nn.Conv2d(64, 3 * (upscale_factor ** 2), kernel_size, stride, pad)
-        
+        out_channels = 3 * (upscale_factor ** 2)
+
+        # 仅 3 层卷积，通道数小，适合 GLSL 逐像素计算
+        self.conv1 = nn.Conv2d(3, 32, kernel_size, stride, pad)
+        self.conv2 = nn.Conv2d(32, 32, kernel_size, stride, pad)
+        self.conv3 = nn.Conv2d(32, out_channels, kernel_size, stride, pad)
+
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
         self._initialize_weights()
 
     def forward(self, x):
         x = self.act(self.conv1(x))
         x = self.act(self.conv2(x))
-        x = self.act(self.conv3(x))
-        x = self.act(self.conv4(x))
-        x = self.act(self.conv5(x))
-        x = self.pixel_shuffle(self.conv6(x))
+        x = self.pixel_shuffle(self.conv3(x))
         x = torch.sigmoid(x)
         return x
 
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                if m == self.conv6:
+                if m is self.conv3:
+                    # 最后一层（PixelShuffle 前）使用较小增益，避免输出值过大
                     init.orthogonal_(m.weight, gain=0.1)
-                elif m in [self.conv1, self.conv2, self.conv3, self.conv4, self.conv5]:
+                elif m is self.conv1 or m is self.conv2:
                     init.orthogonal_(m.weight, init.calculate_gain('leaky_relu', 0.2))
                 else:
                     init.orthogonal_(m.weight)
