15ef841170b9655cfe06eab749be9c733db197d0
RISC-1145 <wtpp647@gmail.com>
2026-04-28 16:16:28 +0800

    Update model.py

--- super_resolution/model.py
+++ super_resolution/model.py
@@ -16,9 +16,10 @@ class Net(nn.Module):
         out_channels = 3 * (upscale_factor ** 2)
 
         # 仅 3 层卷积，通道数小，适合 GLSL 逐像素计算
-        self.conv1 = nn.Conv2d(3, 32, kernel_size, stride, pad)
-        self.conv2 = nn.Conv2d(32, 32, kernel_size, stride, pad)
-        self.conv3 = nn.Conv2d(32, out_channels, kernel_size, stride, pad)
+        self.conv1 = nn.Conv2d(3, 128, kernel_size, stride, pad)
+        self.conv2 = nn.Conv2d(128, 128, kernel_size, stride, pad)
+        self.conv3 = nn.Conv2d(128, 32, kernel_size, stride, pad)
+        self.conv4 = nn.Conv2d(32, out_channels, kernel_size, stride, pad)
 
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
         self._initialize_weights()
@@ -26,17 +27,18 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.act(self.conv1(x))
         x = self.act(self.conv2(x))
-        x = self.pixel_shuffle(self.conv3(x))
+        x = self.act(self.conv3(x))
+        x = self.pixel_shuffle(self.conv4(x))
         x = torch.sigmoid(x)
         return x
 
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                if m is self.conv3:
+                if m is self.conv4:
                     # 最后一层（PixelShuffle 前）使用较小增益，避免输出值过大
                     init.orthogonal_(m.weight, gain=0.1)
-                elif m is self.conv1 or m is self.conv2:
+                elif m is self.conv1 or m is self.conv2 or m is self.conv3:
                     init.orthogonal_(m.weight, init.calculate_gain('leaky_relu', 0.2))
                 else:
                     init.orthogonal_(m.weight)
